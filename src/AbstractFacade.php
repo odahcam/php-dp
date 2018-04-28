8ccbb387609fd61f5117d3a42967ca6ea48f9224
Luiz Machado <machado@odahcam.com>
2018-04-27 21:38:33 -0300

    Update AbstractFacade.php

--- src/AbstractFacade.php
+++ src/AbstractFacade.php
@@ -5,13 +5,6 @@ namespace Odahcam\DP;
 abstract class AbstractFacade extends AbstractSingleton
 {
     /**
-     * The not fake object.
-     *
-     * @var object
-     */
-    protected $faking;
-
-    /**
      * Who is this class faking for, full qualifyied className.
      *
      * @var string
@@ -19,51 +12,14 @@ abstract class AbstractFacade extends AbstractSingleton
     protected static $fakingFor;
 
     /**
-     * Construtor do tipo protegido previne que uma nova instância da
-     * Classe seja criada através do operador `new` de fora dessa classe.
+     * {@inheritDoc}
      */
-    protected function __construct()
+    public function getInstance()
     {
-        $this->faking = new static::$fakingFor;
-    }
-
-    /**
-     *
-     */
-    public function getFaking()
-    {
-        return $this->faking;
-    }
-
-    /**
-     * Handle dynamic, static calls to the object.
-     *
-     * @param  string  $method
-     * @param  array  $args
-     * @return mixed
-     */
-    public static function __callStatic($method, $args)
-    {
-        $instance = static::getInstance()->getFaking();
-
-        switch (count($args)) {
-            case 0:
-                return $instance->{$method}();
-
-            case 1:
-                return $instance->{$method}($args[0]);
-
-            case 2:
-                return $instance->{$method}($args[0], $args[1]);
-
-            case 3:
-                return $instance->{$method}($args[0], $args[1], $args[2]);
-
-            case 4:
-                return $instance->{$method}($args[0], $args[1], $args[2], $args[3]);
-
-            default:
-                return call_user_func_array([$instance, $method], $args);
+        if (static::$instance === null) {
+            static::$instance = new static::$fakingFor;
         }
+
+        return static::$instance;
     }
 }
